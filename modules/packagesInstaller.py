e2edd6b0ea6511263772d9d2e81339347a98b7a0
Oleh Dmytrenko <dmytrenko.o@gmail.com>
2022-02-28 06:57:11 +0200

    README - print (#16)

--- modules/packagesInstaller.py
+++ modules/packagesInstaller.py
@@ -12,17 +12,17 @@ import subprocess
 
 def setup_packeges(packages):
     for package in packages:   
-        print (package)
         if package in sys.modules:
-            print(f"{package!r} already in sys.modules")
+            continue
+            #print(f"{package!r} already in sys.modules")
         elif (spec := importlib.util.find_spec(package)) is not None:
             # If you choose to perform the actual import ...
             module = importlib.util.module_from_spec(spec)
             sys.modules[package] = module
             spec.loader.exec_module(module)
-            print(f"{package!r} has been imported")
+            #print(f"{package!r} has been imported")
         else:
-            print(f"Can't find the {package!r} module")
+            #print(f"Can't find the {package!r} module")
             try:
                 # implement pip as a subprocess:
                 subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
